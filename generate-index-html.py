16f9ef26513b3ebd3b53863af44e6da49b76a5ba
Italo A. Casas <me@italoacasas.com>
2016-06-06 20:55:52 -0400

    improving colors

--- generate-index-html.py
+++ generate-index-html.py
@@ -14,7 +14,7 @@ with open('out/index.html', 'w') as out:
     <title>Node.js Core Coverage</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.blue-indigo.min.css" /> 
+    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.yellow-indigo.min.css" />
     <style media="screen" type"text/css">
       .table-container {
         display: flex;
