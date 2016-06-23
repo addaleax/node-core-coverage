a5551d35c0b3bc7ba1da5544519e0ccabeea77a2
Anna Henningsen <anna@addaleax.net>
2016-06-24 01:29:25 +0200

    Use https:// URLs everywhere
    Update an included font to use `https://`.

--- generate-index-html.py
+++ generate-index-html.py
@@ -13,7 +13,7 @@ with open('out/index.html', 'w') as out:
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Node.js Core Coverage</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
     <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.yellow-indigo.min.css" />
     <style media="screen" type="text/css">
       .table-container {
